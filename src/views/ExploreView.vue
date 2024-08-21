720e0fc3d0fd45cf767e0b170133b2ec4fc45a94
Paola Albarnaz <paola.silva.nh1@gmail.com>
2024-08-21 16:27:52 -0300

    initial commit

---
+++ src/views/ExploreView.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import SideBar from '@/components/SideBar.vue';
+import { getUser} from '@/services/api';
+import { onMounted, ref } from 'vue';
+import type { UserType } from '@/types';
+import { tempoDesdeCriacao } from '@/utils/PastTime';
+import ExploreComponent from '@/components/ExploreComponent.vue';
+import { items } from '@/utils/ExploreArray';
+
+const item = ref<UserType[]>([]);
+
+
+async function handleGetUser() {
+  const userData = localStorage.getItem('userData');
+  if (!userData) {
+    const response = await getUser();
+    item.value = response.data.data;
+    localStorage.setItem('userData', JSON.stringify(item.value));
+    return;
+  }
+  item.value = JSON.parse(userData);
+}
+
+onMounted(() => {
+  handleGetUser();
+});
+
+</script>
+
+<template>
+  <div>
+    <div class="home-container">
+      <div class="home-nav">
+        <SideBar :item="item" />
+      </div>
+      <div class="home-content">
+        <span class="home-content-title">
+          <span> Explorar </span>
+        </span>
+
+        <div class="explore-div" v-for="(item, index) in items" :key="index">
+          <p>{{ item.title }} - {{ tempoDesdeCriacao(item.created_at) }}</p>
+          <h4>{{ item.content }}</h4>
+        </div>
+      </div>
+
+      <ExploreComponent/>
+
+      </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+.explore-div{
+  margin: 1rem;
+  margin-bottom: 1.6rem ;
+}
+
+.explore-div p {
+  font-size: 0.8rem;
+}
+
+.spinner-div{
+  position: absolute;
+  left: 46%;
+  top: 50%
+}
+
+.home-container {
+  display: flex;
+
+  width: 100%;
+  height: 100%;
+  background: #ffffff;
+}
+
+.home-nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 20%;
+  height: 100vh;
+  background: #ffffff;
+  position: sticky;
+  top: 0;
+  color: white;
+}
+
+.home-nav-component {
+  width: 90%;
+  height: 100%;
+  padding: 1em 0 0 1em;
+  background-color: black;
+}
+
+.home-content {
+  width: 55%;
+  height: 100%;
+  border-right: 2px solid #e9e9e9;
+  border-left: 2px solid #e9e9e9;
+}
+
+.home-content-title {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  width: 100%;
+  height: 4rem;
+  border-bottom: 2px solid #e9e9e9;
+  font-weight: 600;
+  font-size: 1.3rem;
+  font-style: normal;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,140 @@
+<script setup lang="ts">
+import SideBar from '@/components/SideBar.vue';
+import ListCard from '@/components/ListCard.vue';
+import { getUser, showPosts } from '@/services/api';
+import type { TweetType } from '@/types/TweetType';
+import { onMounted, ref, computed, onUnmounted } from 'vue';
+import type { UserType } from '@/types';
+import SpinnerComponent from '@/components/SpinnerComponent.vue';
+import ExploreComponent from '@/components/ExploreComponent.vue';
+import ApplicationBar from '@/components/ApplicationBar.vue';
+
+const listenEmit = () => {
+  fetchTweets();
+};
+
+const loadingVisible = ref<boolean>(false);
+const tweets = ref<TweetType[]>([]);
+const item = ref<UserType[]>([]);
+const endpoint = '/posts';
+
+async function fetchTweets() {
+  loadingVisible.value = true;
+  const response = await showPosts(endpoint);
+  loadingVisible.value = false;
+  tweets.value = response.data.data;
+}
+
+async function handleGetUser() {
+  const userData = localStorage.getItem('userData');
+  if (!userData) {
+    const response = await getUser();
+    item.value = response.data.data;
+    localStorage.setItem('userData', JSON.stringify(item.value));
+    return;
+  }
+  item.value = JSON.parse(userData);
+}
+
+onMounted(() => {
+  handleGetUser();
+  fetchTweets();
+});
+
+const windowWidth = ref(window.innerWidth);
+const isMobileView = computed(() => windowWidth.value < 600);
+
+const handleResize = () => {
+  windowWidth.value = window.innerWidth;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize);
+});
+</script>
+
+<template>
+  <div>
+    <div class="home-container">
+      <div class="home-nav">
+        <SideBar :item="item" @call-emit="listenEmit" />
+      </div>
+
+      <!-- Aplica a classe 'full-width' ao home-content se for visualização móvel -->
+      <div class="home-content" :class="{ 'full-width': isMobileView }">
+        <span class="home-content-title">
+          <span>Página Inicial</span>
+        </span>
+
+        <div class="spinner-div d-flex justify-center mt-5">
+          <SpinnerComponent v-if="loadingVisible" color="blue" />
+        </div>
+
+        <ListCard :tweets="tweets" />
+      </div>
+
+      <!-- Esconde o ExploreComponent em telas pequenas -->
+      <ExploreComponent v-if="!isMobileView" />
+    </div>
+  </div>
+  <ApplicationBar />
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+.spinner-div {
+  position: absolute;
+  left: 46%;
+  top: 50%;
+}
+
+.home-container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: #ffffff;
+}
+
+.home-nav {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20%;
+  height: 100vh;
+  background: #ffffff;
+  position: sticky;
+  top: 0;
+  color: white;
+}
+
+.home-content {
+  width: 55%;
+  height: 100%;
+  border-right: 2px solid #e9e9e9;
+  border-left: 2px solid #e9e9e9;
+}
+
+.home-content.full-width {
+  width: 100%;
+}
+
+.home-content-title {
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  width: 100%;
+  height: 4rem;
+  border-bottom: 2px solid #e9e9e9;
+  font-weight: 600;
+  font-size: 1.3rem;
+  font-style: normal;
+}
+</style>
